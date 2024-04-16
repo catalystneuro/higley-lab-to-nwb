70d079698e27636c0540d88dc128ae2926f5cdcf
Alessandra Trapani <alessandramaria.trapani@polimi.it>
2024-04-16 10:31:30 +0200

    add facemap interace and aligned timestamps

--- src/higley_lab_to_nwb/benisty_2022/benisty_2022nwbconverter.py
+++ src/higley_lab_to_nwb/benisty_2022/benisty_2022nwbconverter.py
@@ -4,7 +4,7 @@ from neuroconv import NWBConverter
 from higley_lab_to_nwb.benisty_2022.benisty_2022_spike2signals_interface import Benisty2022Spike2SignalsInterface
 from higley_lab_to_nwb.benisty_2022.benisty_2022_imaginginterface import Benisty2022ImagingInterface
 
-from neuroconv.datainterfaces import VideoInterface
+from neuroconv.datainterfaces import VideoInterface, FacemapInterface
 
 
 class Benisty2022NWBConverter(NWBConverter):
@@ -13,6 +13,7 @@ class Benisty2022NWBConverter(NWBConverter):
     data_interface_classes = dict(
         Spike2Signals=Benisty2022Spike2SignalsInterface, 
         Video=VideoInterface,
+        FacemapInterface=FacemapInterface,
         ImagingBlueExcitationGreenChannel=Benisty2022ImagingInterface,
         ImagingBlueExcitationRedChannel=Benisty2022ImagingInterface,
         ImagingUVExcitationGreenChannel=Benisty2022ImagingInterface,
@@ -24,14 +25,16 @@ class Benisty2022NWBConverter(NWBConverter):
     def temporally_align_data_interfaces(self):
         ttlsignal_interface = self.data_interface_objects["Spike2Signals"]
         video_interface = self.data_interface_objects["Video"]
+        facemap_interface = self.data_interface_objects["FacemapInterface"]
         video_interface._timestamps = video_interface.get_timestamps()
         stream_id = next(
             (
                 stream_id
-                for stream_id, stream_name in ttlsignal_interface.stream_ids_to_names_map.items()
+                for stream_id, stream_name in ttlsignal_interface.ttl_stream_ids_to_names_map.items()
                 if stream_name == "TTLSignalPupilCamera"
             ),
             None,
         )
         ttl_times = ttlsignal_interface.get_event_times_from_ttl(stream_id=stream_id)
         video_interface.set_aligned_starting_time(ttl_times[0])
+        facemap_interface.set_aligned_starting_time(ttl_times[0])
