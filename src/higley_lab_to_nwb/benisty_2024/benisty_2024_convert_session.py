148de926938f80a344c7820c9691c856050a82d8
Alessandra Trapani <alessandramaria.trapani@polimi.it>
2024-05-16 15:42:23 +0200

    minor fixes

--- src/higley_lab_to_nwb/benisty_2024/benisty_2024_convert_session.py
+++ src/higley_lab_to_nwb/benisty_2024/benisty_2024_convert_session.py
@@ -5,7 +5,6 @@ from typing import Union
 from neuroconv.utils import load_dict_from_file, dict_deep_update
 from higley_lab_to_nwb.benisty_2024 import Benisty2024NWBConverter
 import os
-import glob
 
 
 def session_to_nwb(
@@ -17,39 +16,30 @@ def session_to_nwb(
         output_dir_path = output_dir_path / "nwb_stub"
     output_dir_path.mkdir(parents=True, exist_ok=True)
 
-    nwbfile_path = output_dir_path / f"{session_id}_new.nwb"
-
-    source_data = dict()
-    conversion_options = dict()
-
     nwbfile_path = output_dir_path / f"{session_id}.nwb"
 
     source_data = dict()
     conversion_options = dict()
 
     # Add 2p Imaging
-    imaging_path = folder_path / "tiff"
-    source_data.update(dict(TwoPhotonImagingGreenChannel=dict(folder_path=str(imaging_path), file_pattern=f"*.tif")))
+    imaging_path = folder_path / "tiff"    
+    source_data.update(dict(TwoPhotonImagingGreenChannel=dict(folder_path=str(imaging_path), file_pattern="*.tif")))
     conversion_options.update(dict(TwoPhotonImagingGreenChannel=dict(stub_test=stub_test)))
 
     # Add Segmentation
     suite2p_path = folder_path / "suite2p"
-    source_data.update(dict(SegmentationGreenChannel=dict(folder_path = suite2p_path)))
+    source_data.update(dict(SegmentationGreenChannel=dict(folder_path=suite2p_path)))
     conversion_options.update(dict(SegmentationGreenChannel=dict(stub_test=stub_test)))
 
     # Add ophys metadata
     ophys_metadata_path = Path(__file__).parent / "metadata" / "benisty_2024_ophys_metadata.yaml"
     ophys_metadata = load_dict_from_file(ophys_metadata_path)
 
-    converter = Benisty2024NWBConverter(
-        source_data=source_data,ophys_metadata=ophys_metadata
-    )
+    converter = Benisty2024NWBConverter(source_data=source_data, ophys_metadata=ophys_metadata)
 
     # Add datetime to conversion
 
     metadata = converter.get_metadata()
-    date = read_session_start_time(folder_path=folder_path)
-    metadata["NWBFile"]["session_start_time"] = date
     subject_id = session_id.split("_")[1]
     metadata["Subject"].update(subject_id=subject_id)
     metadata["NWBFile"].update(session_id=session_id)
@@ -73,7 +63,7 @@ if __name__ == "__main__":
     output_dir_path = root_path / "Higley-conversion_nwb/"
     stub_test = True
     session_ids = os.listdir(data_dir_path)
-    session_id = "11222019_grabAM06_vis_stim"
+    session_id = "04072021_am2psi_05_spont"
     folder_path = data_dir_path / Path(session_id)
     session_to_nwb(
         folder_path=folder_path,
