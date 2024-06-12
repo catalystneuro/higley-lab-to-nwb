07d0a2b22bad2556f06f4e78ea53e238525bfc99
Alessandra Trapani <alessandramaria.trapani@polimi.it>
2024-06-12 14:34:05 +0200

    lazy load with ScanImageTiffReader

--- src/higley_lab_to_nwb/extractors/mesoscopic_imaging_extractor.py
+++ src/higley_lab_to_nwb/extractors/mesoscopic_imaging_extractor.py
@@ -172,17 +172,19 @@ class MesoscopicImagingExtractor(ImagingExtractor):
         if start_frame is None:
             start_frame = 0
         if end_frame is None:
-            end_frame = self._num_frames
-        self.check_frame_inputs(end_frame)
+            end_frame = self._total_num_frames // self._num_channels
+        end_frame_inclusive = end_frame - 1
+        self.check_frame_inputs(end_frame_inclusive)
         self.check_frame_inputs(start_frame)
         raw_start = self.frame_to_raw_index(start_frame) 
-        raw_end = self.frame_to_raw_index(end_frame) 
+        raw_end_inclusive = self.frame_to_raw_index(end_frame_inclusive) 
+        raw_end = raw_end_inclusive + 1
 
         ScanImageTiffReader = _get_scanimage_reader()
         with ScanImageTiffReader(filename=str(self.file_path)) as io:
             raw_video = io.data(beg=raw_start, end=raw_end)
-
-        return raw_video[::self._num_channels]
+        video = raw_video[::self._num_channels]
+        return video
 
     def get_image_size(self) -> Tuple[int, int]:
         return (self._num_rows, self._num_columns)
