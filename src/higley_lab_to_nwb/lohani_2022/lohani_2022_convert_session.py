1f3d1d650b2fe712f2cf83d83d5018cf7660fdf5
alessandratrapani <alessandramaria.trapani@gmail.com>
2024-08-05 15:38:51 +0200

    remove comments

--- src/higley_lab_to_nwb/lohani_2022/lohani_2022_convert_session.py
+++ src/higley_lab_to_nwb/lohani_2022/lohani_2022_convert_session.py
@@ -144,48 +144,48 @@ def session_to_nwb(
         }
         photon_series_index += 1
 
-    # # Add processed imaging data
-    # processed_imaging_path = parcellation_folder_path / "final_dFoF.mat"
-    # for excitation_type, channel in excitation_type_channel_combination.items():
-    #     process_type = excitation_type.lower() if not excitation_type == "Violet" else "uv"
-    #     suffix = f"{excitation_type}Excitation{channel}Channel"
-    #     interface_name = f"DFFImaging{suffix}"
-    #     source_data[interface_name] = {
-    #         "file_path": processed_imaging_path,
-    #         "sampling_frequency": sampling_frequency,
-    #         "photon_series_type": "OnePhotonSeries",
-    #         "process_type": process_type,
-    #     }
-    #     conversion_options[interface_name] = {
-    #         "stub_test": stub_test,
-    #         "photon_series_index": photon_series_index,
-    #         "photon_series_type": "OnePhotonSeries",
-    #         "parent_container": "processing/ophys",
-    #     }
-    #     photon_series_index += 1
-    #
-    # # Add parcellation output data
-    # mat_file_path = list(parcellation_folder_path.glob(f"green_{session_id}*.mat"))[0]
-    # plane_segmentation_name = "ParcellatedPlaneSegmentation"
-    # source_data.update(
-    #     dict(
-    #         ParcellsSegmentationInterface=dict(
-    #             file_path=mat_file_path,
-    #             sampling_frequency=sampling_frequency,
-    #             image_size=[256, 256],
-    #             plane_segmentation_name=plane_segmentation_name,
-    #         )
-    #     )
-    # )
-    # conversion_options.update(
-    #     dict(
-    #         ParcellsSegmentationInterface=dict(
-    #             include_roi_acceptance=False,
-    #             plane_segmentation_name=plane_segmentation_name,
-    #             stub_test=stub_test,
-    #         )
-    #     )
-    # )
+    # Add processed imaging data
+    processed_imaging_path = parcellation_folder_path / "final_dFoF.mat"
+    for excitation_type, channel in excitation_type_channel_combination.items():
+        process_type = excitation_type.lower() if not excitation_type == "Violet" else "uv"
+        suffix = f"{excitation_type}Excitation{channel}Channel"
+        interface_name = f"DFFImaging{suffix}"
+        source_data[interface_name] = {
+            "file_path": processed_imaging_path,
+            "sampling_frequency": sampling_frequency,
+            "photon_series_type": "OnePhotonSeries",
+            "process_type": process_type,
+        }
+        conversion_options[interface_name] = {
+            "stub_test": stub_test,
+            "photon_series_index": photon_series_index,
+            "photon_series_type": "OnePhotonSeries",
+            "parent_container": "processing/ophys",
+        }
+        photon_series_index += 1
+
+    # Add parcellation output data
+    mat_file_path = list(parcellation_folder_path.glob(f"green_{session_id}*.mat"))[0]
+    plane_segmentation_name = "ParcellatedPlaneSegmentation"
+    source_data.update(
+        dict(
+            ParcellsSegmentationInterface=dict(
+                file_path=mat_file_path,
+                sampling_frequency=sampling_frequency,
+                image_size=[256, 256],
+                plane_segmentation_name=plane_segmentation_name,
+            )
+        )
+    )
+    conversion_options.update(
+        dict(
+            ParcellsSegmentationInterface=dict(
+                include_roi_acceptance=False,
+                plane_segmentation_name=plane_segmentation_name,
+                stub_test=stub_test,
+            )
+        )
+    )
     # Add Behavioral Video Recording
     avi_files = list(folder_path.glob(f"{search_pattern}*.avi"))
     video_file_path = avi_files[0]
