2431aacdf35e5ee1b25102be25c03aa8d7bc0051
alessandratrapani <alessandramaria.trapani@gmail.com>
2024-08-14 12:02:44 +0200

    minor fixes

--- src/higley_lab_to_nwb/lohani_2022/lohani_2022_convert_all_sessions.py
+++ src/higley_lab_to_nwb/lohani_2022/lohani_2022_convert_all_sessions.py
@@ -8,15 +8,16 @@ from nwbinspector import inspect_all
 from nwbinspector.inspector_tools import save_report, format_messages
 
 # Parameters for conversion
-root_path = Path("G:")
-data_dir_path = root_path / "Higley-CN-data-share/Lohani22"
+root_path = Path("F:")
+data_dir_path = root_path / "Higley-CN-data-share"
 output_dir_path = root_path / "Higley-conversion_nwb/"
-stub_test = True
+stub_test = False
 verbose = True
 
 session_folder_format = "{date_string}_{subject_id}_{behavior_type}"
 
-for folder_path in data_dir_path.iterdir():
+raw_imaging_dir_path = data_dir_path / "Lohani22 Meso Data"
+for folder_path in raw_imaging_dir_path.iterdir():
     if folder_path.is_dir():
         session_id = folder_path.name
         metadata = parse(session_folder_format, session_id)
@@ -27,7 +28,7 @@ for folder_path in data_dir_path.iterdir():
         subject_id = metadata["subject_id"]
         parcellation_folder_path = (
             data_dir_path
-            / "parcellation"
+            / "Lohani22 Parcellated data"
             / f"{subject_id.replace('AM', '')}"
             / "imaging with 575 excitation"
             / session_id
